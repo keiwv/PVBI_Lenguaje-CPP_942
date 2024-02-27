832def932f4bb70d8318ad6ca54f1f4bce4e40c9
Brayan Ivan <brayan.perez56@uabc.edu.mx>
2024-02-27 10:20:57 -0800

    Add new folder for class work     Learning how to use classes with vectors and operators

---
+++ Practica_Clase/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include "vector.h"
+
+using namespace std;
+
+int main()
+{
+    Vector v1(10, 20, 5), v2 = {25, 11, 17};
+    Vector result;
+
+    result = v1.Multiplicar(v2);
+
+    result = v1 * v2;
+    cout << result.ToString();
+    result = v1 * 2;
+    cout << result.ToString();
+}
---
+++ Practica_Clase/vector.h
@@ -0,0 +1,86 @@
+#pragma once
+#include <sstream> // streams basados en cadena
+using std::string;
+
+class Vector
+{
+public:
+    double x;
+    double y;
+    double z;
+    Vector(double x = 0, double y = 0, double z = 0);
+    string ToString();
+
+public:
+    Vector Sumar(Vector v2);
+    Vector Multiplicar(Vector v2);
+    Vector Multiplicar(double s);
+    Vector operator*(Vector v2);
+    Vector operator*(double s);
+};
+
+Vector::Vector(double x, double y, double z)
+{
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
+
+Vector Vector::Sumar(Vector v2)
+{
+    Vector result;
+
+    result.x = this->x + v2.x;
+    result.y = this->y + v2.y;
+    result.z = this->z + v2.z;
+
+    return result;
+}
+
+string Vector::ToString()
+{
+    /*char cad[128];
+    sprintf(cad, "(%.2lf),(%.2lf),(%.2lf)", x, y, z);*/
+    std::ostringstream out;
+    out << "(" << x
+        << "," << y << ","
+        << z << ")";
+    return out.str();
+}
+
+Vector Vector::Multiplicar(Vector v2)
+{
+    Vector r;
+    r.x = this->y * v2.z - this->z * v2.y;
+    r.y = this->z * v2.x - this->x * v2.z;
+    r.z = this->x * v2.y - this->y * v2.x;
+    return r;
+}
+
+Vector Vector::Multiplicar(double s)
+{
+    Vector r;
+    r.x = this->x * s;
+    r.y = this->y * s;
+    r.z = this->z * s;
+    return r;
+}
+
+Vector Vector::operator*(Vector v2)
+{
+
+    Vector r;
+    r.x = this->y * v2.z - this->z * v2.y;
+    r.y = this->z * v2.x - this->x * v2.z;
+    r.z = this->x * v2.y - this->y * v2.x;
+    return r;
+}
+
+Vector Vector::operator*(double s)
+{
+    Vector r;
+    r.x = this->x * s;
+    r.y = this->y * s;
+    r.z = this->z * s;
+    return r;
+}
