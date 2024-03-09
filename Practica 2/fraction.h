38380507468658e0445036dd38005bf1e1ba8b13
Brayan Ivan <brayan.perez56@uabc.edu.mx>
2024-03-09 15:43:22 -0800

    Operator overloading practice done.

--- Practica 2/fraction.h
+++ Practica 2/fraction.h
@@ -2,24 +2,29 @@
 
 #include <string>
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 class Fraction
 {
-protected:
+public:
     int den;
     int num;
 
 public:
     Fraction(const char *fraction_);
+    Fraction(int num, int deno);
     Fraction operator+(Fraction frac);
     Fraction operator-(Fraction frac);
     Fraction operator/(Fraction frac);
     Fraction operator*(Fraction frac);
     Fraction operator+(int num);
+    Fraction operator-(int num);
+    Fraction operator/(int num);
+    Fraction operator*(int num);
 
-    void display();
+    friend ostream &operator<<(ostream &os, const Fraction &frac);
 };
 string remove_space(const string &str);
 
@@ -28,6 +33,12 @@ Fraction::Fraction(const char *fraction_)
     string result;
     result = remove_space(fraction_);
     sscanf(result.c_str(), "%d/%d", &this->num, &this->den);
+}
+
+Fraction::Fraction(int num, int deno)
+{
+    this->num = num;
+    this->den = deno;
 }
 
 Fraction Fraction::operator+(Fraction frac)
@@ -115,11 +126,108 @@ Fraction Fraction::operator*(Fraction frac)
     return result;
 }
 
-void Fraction::display()
+Fraction Fraction::operator+(int num)
 {
-    cout << this->num;
-    cout << "\\";
-    cout << this->den << endl;
+    Fraction result("0/0");
+    if (num == 1)
+    {
+        num = this->den;
+        result.num = num + this->num;
+        result.den = num;
+    }
+    else
+    {
+        num = num * this->den;
+        result.den = this->den;
+        result.num = num + this->num;
+    }
+
+    return result;
+}
+
+Fraction Fraction::operator-(int num)
+{
+    Fraction result("0/0");
+    if (num == 1)
+    {
+        num = this->den;
+        result.num = num - this->num;
+        result.den = num;
+    }
+    else
+    {
+        num = num * this->den;
+        result.den = this->den;
+        result.num = num - this->num;
+    }
+
+    return result;
+}
+
+Fraction Fraction::operator/(int num)
+{
+    Fraction result("0/0");
+
+    if (num == 1)
+    {
+        result.den = this->den;
+        result.num = this->num;
+    }
+    else
+    {
+        result.num = this->num;
+        result.den = this->den * num;
+    }
+    return result;
+}
+
+Fraction Fraction::operator*(int num)
+{
+    Fraction result("0/0");
+    result.num = num * this->num;
+    result.den = this->den;
+    return result;
+}
+
+Fraction operator/(int num, Fraction frac)
+{
+    Fraction result("0/0");
+    result.num = num * frac.den;
+    result.den = frac.num;
+    return result;
+}
+
+Fraction operator-(int num, Fraction frac)
+{
+    Fraction result("0/0");
+    result.num = num * frac.den - frac.num;
+    result.den = frac.den;
+    return result;
+}
+
+Fraction operator*(int num, Fraction frac)
+{
+    Fraction result("0/0");
+    result.num = num * frac.num;
+    result.den = frac.den;
+    return result;
+}
+
+ostream &operator<<(ostream &os, const Fraction &frac)
+{
+    int whole_part = frac.num / frac.den;
+    int remainder = frac.num % frac.den;
+
+    if (whole_part != 0)
+        os << whole_part << " ";
+
+    if (remainder != 0)
+        os << remainder << "/" << frac.den;
+
+    if (whole_part == 0 && remainder == 0)
+        os << "0";
+
+    return os;
 }
 
 string remove_space(const string &str)
--- Practica 2/main.cpp
+++ Practica 2/main.cpp
@@ -6,16 +6,16 @@ using namespace std;
 int main()
 {
     Fraction result("0/0");
-    Fraction v1("1/2");
-    Fraction v2("3/25");
+    Fraction v1(3, 4);
+    Fraction v2("2/4");
 
-    result = v1 - v2;
-    result.display();
-
-
-    result = v1 / v2;
-    result.display();
+    result = v1 + v2;
+    cout << v1 << " + " << v2 << " = " << result << endl;
+    v1 = "1/2";
 
     result = v1 * v2;
-    result.display();
+    cout << v1 << " * " << v2 << " = " << result << endl;
+
+    result = v1 * 5;
+    cout << v1 << " * " << "5" << " = " << result << endl;
 }
